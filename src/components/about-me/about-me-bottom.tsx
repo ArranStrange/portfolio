9d3c4d9f122a7fa56584f8bfaba9c4c9be705aa2
Arran <arranstrange@googlemail.com>
2023-12-16 11:17:16 +0000

    Motion Framer & react intersection observer fade in added to about-me

--- src/components/about-me/about-me-bottom.tsx
+++ src/components/about-me/about-me-bottom.tsx
@@ -1,9 +1,36 @@
 import "./about-me.css";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+
+// Motion Framer object to control visible & hidden
+const fadeInVariants = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 5 } },
+  hidden: { opacity: 0, scale: 1 },
+};
 
 function AboutMeBottom() {
+  // import Motion Framer useAnimation hook
+  // import inView from react-intersection-observer to ref view point - returns boolean true if in view, false if not.
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    // if statement to set use object visible when useInView hoot return true
+    if (inView) {
+      controls.start("visible");
+    }
+    // dependencies
+  }, [controls, inView]);
+
   return (
     <div className="about-me-bottom-container">
-      <div className="about-me-bottom-content">
+      <motion.div
+        ref={ref}
+        animate={controls}
+        initial="hidden"
+        variants={fadeInVariants}
+        className="about-me-bottom-content"
+      >
         <p>
           Hello there! I'm a versatile and enthusiastic conversationalist,
           fluent in the language of technology and eager to assist you on your
@@ -23,7 +50,7 @@ function AboutMeBottom() {
           continue this exciting journey together, exploring the vast landscape
           of technology and weaving your success story one command at a time!
         </p>
-      </div>
+      </motion.div>
     </div>
   );
 }
--- src/components/about-me/about-me-middle.tsx
+++ src/components/about-me/about-me-middle.tsx
@@ -1,11 +1,38 @@
 import "./about-me.css";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+
+// Motion Framer object to control visible & hidden
+const fadeInVariants = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 5 } },
+  hidden: { opacity: 0, scale: 1 },
+};
 
 function AboutMeMiddle() {
+  // import Motion Framer useAnimation hook
+  // import inView from react-intersection-observer to ref view point - returns boolean true if in view, false if not.
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    // if statement to set use object visible when useInView hoot return true
+    if (inView) {
+      controls.start("visible");
+    }
+    // dependencies
+  }, [controls, inView]);
+
   return (
     <div className="about-me-middle-container">
-      <div className="about-me-middle-content">
+      <motion.div
+        ref={ref}
+        animate={controls}
+        initial="hidden"
+        variants={fadeInVariants}
+        className="about-me-middle-content"
+      >
         <h1>UK BASED FRONTEND DEV & UI/UX DESIGNER</h1>
-      </div>
+      </motion.div>
     </div>
   );
 }
