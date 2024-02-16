19a6f6121613ee2449a76433c5bf308ca48b05a1
Arran <arranstrange@googlemail.com>
2024-02-16 17:19:55 +0000

    arrow edts

--- src/components/frontend/frontend-middle.tsx
+++ src/components/frontend/frontend-middle.tsx
@@ -66,15 +66,21 @@ function FrontendMiddle() {
       <motion.h2
         initial={{ opacity: 0 }}
         animate={inView ? { opacity: 1 } : { opacity: 0 }}
-        transition={{ duration: 1, delay: 0.8 }}
+        transition={{ duration: 1, delay: 1 }}
         style={{ fontSize: "9rem" }}
         className="title-right"
       >
         JAVASCRIPT
       </motion.h2>
-      <div>
+      <motion.div
+        className="arrow"
+        initial={{ y: "-100vh", x: -20, opacity: 0 }}
+        animate={inView ? { y: 0, opacity: 1 } : { y: -1000 }}
+        transition={{ delay: 0.2, duration: 1, type: "tween" }}
+        style={{ marginBottom: "25px" }}
+      >
         <MdOutlineKeyboardDoubleArrowDown className="arrow" />
-      </div>
+      </motion.div>
     </motion.div>
   );
 }
